7545b01fd8e024048f053eb05a52a0f07e72e246
Bilolbek <124268054+delolipop777@users.noreply.github.com>
2025-08-02 16:21:06 +0500

    INFRA: Initialize Unit Test Project

---
+++ Nestly.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,21 @@
+﻿// |====================================================|
+// |Copyright©                                          |
+// |Nestly API - Home Finder                            |
+// |Modern API to discover homes for rent  or purchase  |
+// |                                                    | 
+// |----------------------------------------------------|
+// |                                                    | 
+// |All rights reserved, including the right to         |
+// |reproduce this work in any form. For permission     |
+// |requests, contact [Your Contact Information]        |
+// |====================================================|
+
+
+namespace Nestly.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
